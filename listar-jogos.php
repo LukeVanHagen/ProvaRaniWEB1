7dc10208100480d08baf52214b37e28f27b127ca
LukeVanHagen <flgs@discente.ifpe.edu.br>
2023-02-02 13:01:29 -0300

    só mais um códigozinho

---
+++ listar-jogos.php
@@ -0,0 +1,46 @@
+
+    
+    <h1> Lista de jogos para Maratonar  </h1>
+
+    <table class='table table-hover 
+            table-striped table-bordered'>
+                <tr>
+                        <th>Nome</th>
+                        <th>Plataforma</th>
+                        <th>Gênero</th>
+                        <th>Data de Lançamento</th>
+                </tr> 
+
+                <tr>
+                    <td>DDTank Brasil </td>
+                    <td> Mobile / Windows </td>
+                    <td> Aventura / Rpg </td>
+                    <td> 26 de Novembro de 2010</td>
+                </tr>
+
+                <tr>
+                    <td> </td>
+                    <td> </td>
+                    <td> </td>
+                    <td> </td>
+                </tr>
+
+                <tr>
+                    <td> </td>
+                    <td> </td>
+                    <td> </td>
+                    <td> </td>
+                </tr>
+
+                <tr>
+                    <td> </td>
+                    <td> </td>
+                    <td> </td>
+                    <td> </td>
+                </tr>
+    </table>
+
+
+
+
+
